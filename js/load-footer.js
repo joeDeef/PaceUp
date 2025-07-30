ed7521d1fec3530aac392a0315df25aee24eeb50
Joel Defaz <joe.def2019@gmail.com>
2025-07-30 15:08:10 -0500

    Refactor: Eliminación de archivos no usados

---
+++ js/load-footer.js
@@ -0,0 +1,12 @@
+async function loadFooter() {
+  try {
+    const res = await fetch("./components/footer.html");
+    if (!res.ok) throw new Error("No se pudo cargar el footer");
+    const footerHTML = await res.text();
+    document.getElementById("footer-container").innerHTML = footerHTML;
+  } catch (error) {
+    console.error("Error cargando footer:", error);
+  }
+}
+
+loadFooter();
